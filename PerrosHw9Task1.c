b179d131e672ec9124a8c625166fe6a9f7745c2b
Daniel Tayler <danieltayler@mail.weber.edu>
2017-04-04 16:07:50 +0000

    first commit

---
+++ PerrosHw9Task1.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  PerrosHw9Task1.c
+ *       	Usage:  ./PerrosHw9Task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/04/2017 04:06:58 PM
+ *
+ Compiler:  gcc -Wall -Werror -lm 
+ *         Author:  Daniel Tayler (), danieltayler@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int  main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
