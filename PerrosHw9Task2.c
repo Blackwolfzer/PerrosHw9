70d6d8bffe9439494a997abd6913bb2b613e48c0
Sharon Dansie <sharondansie@mail.weber.edu>
2017-04-08 03:23:49 +0000

    Finishing touches, task 2 done

--- PerrosHw9Task2.c
+++ PerrosHw9Task2.c
@@ -18,7 +18,7 @@
 #define ROWS 4
 /* Function Prototypes */
 void MathFunction(float xAxis[], float yAxis[], int Rep);
- void MathFunctionBegin(int xyAxis[][COLS],float x[],float y[]);
+void MathFunctionBegin(int xyAxis[][COLS],float x[],float y[]);
 
 /* Main Program */
 int main(int argc, char *argv[])
@@ -29,14 +29,13 @@ int main(int argc, char *argv[])
 
 	if(inFile != NULL)
 	{
-		printf("<%s> was read.\n", "zone1.txt");
-	
-		for( int j =0; j < ROWS ; j++)
+		//printf("\nThe <%s> file was read.\n", "zone1.txt");
+		for(int j = 0; j < ROWS; j++)
 		{
-			for( int i =0; i < COLS ; i++)
+			for(int i =0; i < COLS; i++)
 			{
-					fscanf(inFile, "%d", &xyValues[j][i]);
-					//printf("%d\n", xyValues[j][i]);
+				fscanf(inFile, "%d", &xyValues[j][i]);
+				//printf("%d\n", xyValues[j][i]);
 			}
 		}
 	}
@@ -53,11 +52,11 @@ int main(int argc, char *argv[])
 /* Function Defenitions */
 void MathFunctionBegin(int xyAxis[][COLS],float x[],float y[])
 {
-	for ( int i = 0; i < ROWS ; i++)
+	for (int i = 0; i < ROWS; i++)
 	{
 		x[i] = xyAxis[i][0];
 		
-		for ( int j = 1; j < COLS ; j++)
+		for (int j = 1; j < COLS; j++)
 		{
 			y[i] = xyAxis[i][j];
 		}
@@ -70,9 +69,9 @@ void MathFunction(float xAxis[], float yAxis[], int Rep)
 {
 	float m = 0.0;
 	float b = 0.0;
-	float sumx = 0.0; //sum of teh x axis
+	float sumx = 0.0; //sum of the x axis
 	float sumy = 0.0; //sum of the y axis
-	float sumxy = 0.0; //sum of hte x time y axis
+	float sumxy = 0.0; //sum of the x time y axis
 	float sumxx = 0.0; // sum of x^2
 	float sumxThenSquare = 0.0; // sum of x then x squared
 	
@@ -81,16 +80,19 @@ void MathFunction(float xAxis[], float yAxis[], int Rep)
 		sumx += xAxis[i];
 		sumy += yAxis[i];
 		sumxy += (xAxis[i] * yAxis[i]);
-		sumxx += ( xAxis[i] * xAxis[i]);
+		sumxx += (xAxis[i] * xAxis[i]);
 	}
-	sumxThenSquare = ( sumx * sumx);
+	sumxThenSquare = (sumx * sumx);
 	// time fore math to work out lets see if we can't gt m and b muahahhaha
-	m = (  (  (sumx * sumy) - (Rep * sumxy)  ) /  (  ( sumxThenSquare ) - ( Rep * sumxx)  )  );
+	m = (((sumx * sumy) - (Rep * sumxy)) / ((sumxThenSquare) - (Rep * sumxx)));
 
-	b = (  (  ( sumx * sumxy)  -  ( sumxx * sumy ) ) /  (  ( sumxThenSquare ) - ( Rep *  sumxx) ) );
+	b = (((sumx * sumxy) - (sumxx * sumy)) /  ((sumxThenSquare) - (Rep *  sumxx)));
 	
-	printf("\nThe Range of altitudes that we were wonderfully given in km are: %.2f to %.2f\n", xAxis[0], xAxis[(ROWS -1)]);
-	printf("\nozone-mix-ratio = %.2f altitude + %.2f\n\n", m , b);
+	printf("\nThe Range of altitudes that we were\n");
+	printf("wonderfully given in km are:\n");
+	printf("%.2f to %.2f\n", xAxis[0], xAxis[(ROWS -1)]);
+	printf("\nLinear model:\n");
+	printf("ozone-mix-ratio = %.2f altitude + %.2f\n\n", m , b);
 
 	return;
 }
